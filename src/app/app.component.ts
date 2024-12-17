ac2304533c896e3139c6010806f0c40685db89b7
Riza Sakhi <rizasakhi3@gmail.com>
2024-12-17 12:41:41 +0700

    membuat dan menambahkan component footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HeroesComponent } from "./heroes/heroes.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, HeroesComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  imports: [],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+
+export class HeroesComponent {
+
+}
