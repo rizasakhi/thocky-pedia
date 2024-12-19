62f1539805cf13f1e256713453db54f8ac424826
Riza Sakhi <rizasakhi3@gmail.com>
2024-12-20 00:25:03 +0700

    menambahkan about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HeroesComponent},
+    {path: 'about', component: AboutComponent}
+];
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
